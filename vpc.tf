ef0ab52ad027857dbfed681f5887004039a8a5e1
Asmita Raut <rautasmita31@gmail.com>
2026-02-18 20:15:21 +0530

    Fixed Issue

--- vpc.tf
+++ vpc.tf
@@ -4,6 +4,7 @@ resource "aws_vpc" "myvpc" {
 resource "aws_subnet" "subnet2" {
   vpc_id = aws_vpc.myvpc.id
   cidr_block = "10.10.2.0/24"
+}
 resource "aws_subnet" "subnet1" {
   vpc_id = aws_vpc.myvpc.id
   cidr_block = "10.10.1.0/24"
